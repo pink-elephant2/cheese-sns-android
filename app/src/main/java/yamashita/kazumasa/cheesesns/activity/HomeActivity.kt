e930246bf57ed096f3f9c5305a3050df5a7925e5
山下 数政 <yamashita-kazu@gnavi.co.jp>
2019-04-08 09:22:07 +0900

    旧端末での修正

--- app/src/main/java/yamashita/kazumasa/cheesesns/activity/HomeActivity.kt
+++ app/src/main/java/yamashita/kazumasa/cheesesns/activity/HomeActivity.kt
@@ -4,13 +4,32 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.View
 import yamashita.kazumasa.cheesesns.R
+
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(yamashita.kazumasa.cheesesns.R.layout.activity_home)
+        val toolbar = findViewById<View>(R.id.tool_bar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        // TODO API Serviceインスタンス化
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // TODO API 通信
+
+        // TODO レイアウト反映
+        initView()
+    }
+
+    private fun initView() {
+
     }
 
     companion object {
